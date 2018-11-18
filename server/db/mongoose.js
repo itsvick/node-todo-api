e4f7bae9c26ed565e313c7314e4b78c8d3aa81a8
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-11-18 21:09:07 +0530

    Added env file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vivek-kasture:vivek555@ds211694.mlab.com:11694/todoapp'  || 'mongodb://localhost:27017/TodoApp');
+import dotenv from 'dotenv';
 
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/TodoApp')
+    .then(() => {
+        console.log('database connected');
+    })
+    .catch(() => {
+        console.log('Unable to connect to DB');
+    });
+// 'mongodb://vivek-kasture:vivek555@ds211694.mlab.com:11694/todoapp'
 module.exports = {
     mongoose
 }
