84466cd157eed25357e26471717a3cd453fb3b2d
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-11-18 20:09:48 +0530

    test server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vivek-kasture:vivek@555@ds211694.mlab.com:11694/todoapp'  || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://vivek-kasture:vivek555@ds211694.mlab.com:11694/todoapp'  || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
