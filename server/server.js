a3fe19c2012833b51b480727976ac9da6751c563
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-11-18 13:45:30 +0530

    Add GET /Todos on route

--- server/server.js
+++ server/server.js
@@ -29,6 +29,17 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        console.log('Find Result', todos);
+        res.send({ todos });
+
+    }).catch((error) => {
+        console.log('Unable to find', error);
+        res.status(400).send(error);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 30000');
 
